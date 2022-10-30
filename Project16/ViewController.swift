430ee3b72292dbb5588e7065360546745cc1ecfa
thibaudg1 <96113614+thibaudg1@users.noreply.github.com>
2022-10-30 18:12:19 +0200

    Added location services

--- Project16/ViewController.swift
+++ Project16/ViewController.swift
@@ -9,14 +9,20 @@ import UIKit
 import MapKit
 import Contacts
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet var mapView: MKMapView!
     
     var searchResults = [MKMapItem]()
     var resultAnnotations = [MKPointAnnotation]()
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() // add key in info.plistas well
+
         
         title = "Capital Cities"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(changeMapType))
@@ -34,14 +40,33 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.addAnnotations([oslo, paris, rome, washington, riga])
         
         
+        // Adding a Placemark to the map:
         let coords = CLLocationCoordinate2DMake(51.5083, -0.1384)
         let address = [CNPostalAddressStreetKey: "181 Piccadilly, St. James's", CNPostalAddressCityKey: "London", CNPostalAddressPostalCodeKey: "W1A 1ER", CNPostalAddressISOCountryCodeKey: "GB"]
-
+        
         let place = MKPlacemark(coordinate: coords, addressDictionary: address)
-
+        
         mapView.addAnnotation(place)
+        
+        // Adding directions to the map:
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 56.948889, longitude: 24.106389), addressDictionary: nil))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 59.437222, longitude: 24.745278), addressDictionary: nil))
+        request.requestsAlternateRoutes = true
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+            
+            for route in unwrappedResponse.routes {
+                self.mapView.addOverlay(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+        }
     }
-
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else { return nil }
         
@@ -67,7 +92,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
             let title = capital.title
             let message = capital.info
             
-
+            
             
             if let vc = storyboard?.instantiateViewController(withIdentifier: "WebView") as? WebViewController {
                 vc.city = title
@@ -76,9 +101,9 @@ class ViewController: UIViewController, MKMapViewDelegate {
                 
             } else {
                 let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    
+                
                 ac.addAction(UIAlertAction(title: "OK", style: .default))
-    
+                
                 present(ac, animated: true)
             }
         }
@@ -119,8 +144,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
             
             search.start { response, error in
                 guard let response = response else {
-                        print("Error: \(error?.localizedDescription ?? "Unknown error").")
-                        return
+                    print("Error: \(error?.localizedDescription ?? "Unknown error").")
+                    return
                 }
                 
                 self?.searchResults = response.mapItems
@@ -146,6 +171,35 @@ class ViewController: UIViewController, MKMapViewDelegate {
             
             resultAnnotations.append(newAnnotation)
             mapView.addAnnotation(newAnnotation)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = UIColor.blue
+        return renderer
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let userLocation = MKPointAnnotation()
+            userLocation.coordinate = location.coordinate
+            userLocation.title = "You"
+            
+            mapView.addAnnotation(userLocation)
+            
+            print("Your location: \(location)")
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // start location services only once you know for sure that your app is authorized to use such services
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            locationManager.requestLocation()
         }
     }
 }
